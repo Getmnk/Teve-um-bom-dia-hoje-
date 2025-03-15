d13e02334364ee4d223352ee893ac478a3b65432
Getmnk <sodafella360@gmail.com>
2025-03-15 13:18:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,16 @@ def main():
     pertencimento = st.number_input("Digite de 1 até 10 seu sentimento de Pertencimento hoje", min_value=1, max_value=10, step=1)
 
     if st.button("Prever"):
-        re1 = sono / 30
+        re1 = sono / 10  # Ajustando para uma escala de 0 a 1
         re2 = interacoes / 10
-        re3 = resiliencia / 20
-        re4 = pertencimento / 40
-        re5 = re1 + re2 + re3 + re4
-        re6 = re5 * 100
+        re3 = resiliencia / 10
+        re4 = pertencimento / 10
+
+        re5 = re1 + re2 + re3 + re4  
+        re6 = re5 * 100  
         re7 = re6 - 104.5 
-        re8 = re7 / 34.872
-        zscore = 0.77696 + 1.2566 * re8
+        re8 = re7 / 34.872  
+        zscore = 0.77696 + 1.2566 * re8  
 
         st.write("re1:", re1)
         st.write("re2:", re2)
@@ -33,21 +34,18 @@ def main():
         st.write("re8:", re8)
         st.write("zscore:", zscore)
 
-        if zscore < 0:
-            result = (1 + np.exp(-zscore)) / 1  
+        result = 1 / (1 + np.exp(-zscore))  
+
+        st.write("Resultado da fórmula de regressão logística:", result)
+
+        if result >= 0.5:
+            st.success('Resultado da previsão: Você teve um Bom Dia! (ISSO É UM TESTE, PODE DAR RESULTADOS NÃO PRECISOS)')
         else:
-            result = (1 + np.exp(zscore)) / 1 
-
-        st.write("Resultado da fórmula (1 + e^-zscore) / 1:", result)
-
-        st.success('Resultado da previsão: {}'.format(
-            "Você teve um Bom Dia! (ISSO É UM TESTE, PODE DAR RESULTADOS NÃO PRECISOS)" 
-            if result >= 50.0
-            else "Você não teve um bom dia. (ISSO É UM TESTE, PODE DAR RESULTADOS NÃO PRECISOS)"
-        ))
+            st.success('Resultado da previsão: Você não teve um bom dia. (ISSO É UM TESTE, PODE DAR RESULTADOS NÃO PRECISOS)')
+        
         print(result)
 
 if __name__ == '__main__':
+    welcome()
     main()
-
 
